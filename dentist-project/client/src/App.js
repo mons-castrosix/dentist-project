5f08d72182b5ca05a1c51b91864158649637a4bd
mons-castrosix <monse.castrosixtos@gmail.com>
2024-06-06 11:02:13 -0600

    Carpetas cliente servidor

---
+++ dentist-project/client/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from  'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+        
+
+
+      <h1>Example heading <span className="badge badge-secondary">New</span></h1>
+<Button variant="primary">Prueba</Button>
+    </div>
+  );
+}
+
+export default App;
